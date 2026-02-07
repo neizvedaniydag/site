4e173081aba222180db4e31a5dbc6ef596f3a2e1
neizvedaniydag <62785983+neizvedaniydag@users.noreply.github.com>
2026-02-07 12:34:43 +0300

    Delete education_platform1 directory

--- education_platform1/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret-key-change-in-production')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///education_platform.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    GIGACHAT_CREDENTIALS = os.getenv('GIGACHAT_CREDENTIALS')
